0797460c49e55eb3f60d977b12b60f159dde52ee
Robert <robertariascastro@outlook.com>
2023-03-03 18:48:09 -0600

    init

---
+++ web/sites/default/settings.secrets.php
@@ -0,0 +1,13 @@
+<?php
+
+$databases['default']['default'] = array(
+  'database' => 'drupal10',
+  'username' => 'drupal10',
+  'password' => 'drupal10',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '3306',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
